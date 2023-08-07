cc86c2f9b26ec23e07dd6b3c2023d676219a37c2
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-08-07 20:00:34 +0400

    init

---
+++ src/stories/Rating.stories.ts
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Rating from "./Rating";
+
+const meta = {
+  title: "Rating",
+  component: Rating,
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Rating>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
