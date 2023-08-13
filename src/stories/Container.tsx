f2baf75e74661a83ab4bf61055004c53e07e0aab
Siddhant Madhur <siddhant.madhur@gmail.com>
2023-08-13 21:08:23 +0400

    test for last commit

--- src/stories/Container.tsx
+++ src/stories/Container.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Rating, { RatingProps } from "../Rating";
 
 function Container(props: RatingProps) {
+  const [value, setValue] = useState(0);
 
-    const [value, setValue] = useState(0)
+  useEffect(() => {
+    setInterval(() => {
+      setValue((e) => (e >= 5 ? 0 : e + 0.5));
+    }, 2000);
+  }, []);
 
   return (
     <div>
